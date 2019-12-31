88d0756126af87c0dadc5f90eafa853fc2a055b2
Evgeny Lipadat <direct.000@yandex.ru>
2019-12-31 10:41:10 +0300

    Initial Commit

---
+++ HealthyTests/HealthyTests.swift
@@ -0,0 +1,34 @@
+//
+//  HealthyTests.swift
+//  HealthyTests
+//
+//  Created by Lipadat Evgeniy on 31/12/2019.
+//  Copyright © 2019 Lipadat Evgeniy. All rights reserved.
+//
+
+import XCTest
+@testable import Healthy
+
+class HealthyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
